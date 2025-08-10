75b1b399903d342b79bdf27602e18a5de36630a1
Donatus <Donaemeka92@gmail.com>
2025-08-10 08:26:43 +0200

    Updated Terraform modules with new security rules

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "aws" {
 
 module "vpc" {
   source     = "../terraform-modules/vpc"
-  cidr_block = var.cidr_block  # Changed from var.vpc_cidr to match module
+  cidr_block = var.cidr_block  
   vpc_name   = var.vpc_name
 }
 
 module "subnets" {
   source             = "../terraform-modules/subnets"
   vpc_id             = module.vpc.vpc_id
-  public_subnets     = var.public_subnets    # Now matches list type
-  private_subnets    = var.private_subnets   # Now matches list type
+  public_subnets     = var.public_subnets    
+  private_subnets    = var.private_subnets   
   availability_zones = var.availability_zones
 }
 
